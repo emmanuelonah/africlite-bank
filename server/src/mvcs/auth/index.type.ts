6d3063fd1ad0e43e23fb384e971ba5d081b7f438
emmanuelonah <emmajerry2016@gmail.com>
2023-10-11 12:15:06 +0200

    Feature: added new feature

--- server/src/mvcs/auth/index.type.ts
+++ server/src/mvcs/auth/index.type.ts
@@ -1,1 +1,24 @@
-export {};
+import { Schema } from 'mongoose';
+
+type SharedTypes = {
+    lastName: string;
+    firstName: string;
+    dob: string;
+    email: string;
+    password: string;
+    phone: string;
+    address: string;
+    taxId: string;
+    personalIdType: string;
+    personalIdNo: string;
+};
+
+export interface SignupRequestI extends SharedTypes {}
+
+export interface SignupResponseI extends SharedTypes {
+    userRef: Schema.Types.ObjectId;
+    bankName: string;
+    bankBranch: string;
+    iban: string;
+    bic: string;
+}

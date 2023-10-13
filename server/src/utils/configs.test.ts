7ad423ce481957d67ca6bdf9f8cdc1153229afcf
Emmanuel Onah <37575223+emmanuelonah@users.noreply.github.com>
2023-10-14 00:15:36 +0200

    Update: fixed test

--- server/src/utils/configs.test.ts
+++ server/src/utils/configs.test.ts
@@ -4,7 +4,7 @@ describe('configs', () => {
     it('should return configuration object', () => {
         expect(configs.serverPort).toBe(8080);
         expect(configs.clientPort).toBe(4000);
-        expect(configs.serverUrl).toBe('http://localhost:8080/');
+        expect(configs.serverUrl).toBe('https://localhost:8080/');
         expect(configs.clientUrl).toBe('http://localhost:4000/');
         expect(typeof configs.serverDbUri).toBe('string');
     });

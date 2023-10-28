11a6f4e841944997fec7148a11d996aa13e37175
emmanuelonah <emmajerry2016@gmail.com>
2023-10-28 23:30:46 +0200

    Feature: added reusable internationalization initializer

--- client/i18n/src/index.ts
+++ client/i18n/src/index.ts
@@ -1,1 +1,19 @@
-export {};
+import i18n from 'i18next';
+
+import { initReactI18next } from 'react-i18next';
+
+export type Resources = {
+    [langCode: string]: {
+        translation: Record<string, string>;
+    };
+};
+
+export function initializeI18n(resources: Resources, lng: string) {
+    i18n.use(initReactI18next).init({
+        resources,
+        lng,
+        interpolation: {
+            escapeValue: false,
+        },
+    });
+}
